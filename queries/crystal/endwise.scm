a4c5179e8f8bf84f379e867e28f3ddc883f40ece
Karol Ostrowski <xlolsonx@gmail.com>
2026-04-22 00:00:28 +0200

    Use correct Crystal grammar node names

--- queries/crystal/endwise.scm
+++ queries/crystal/endwise.scm
@@ -1,71 +1,41 @@
 ((module_def name: (_) @cursor) @endable @indent (#endwise! "end"))
-
 ((class_def name: (_) @cursor superclass: (_)? @cursor) @endable @indent (#endwise! "end"))
-
-((struct_def name: (_) @cursor) @endable @indent (#endwise! "end"))
-
+((struct_def name: (_) @cursor superclass: (_)? @cursor) @endable @indent (#endwise! "end"))
 ((enum_def name: (_) @cursor) @endable @indent (#endwise! "end"))
-
 ((lib_def name: (_) @cursor) @endable @indent (#endwise! "end"))
-
 ((union_def name: (_) @cursor) @endable @indent (#endwise! "end"))
-
+((annotation_def name: (_) @cursor) @endable @indent (#endwise! "end"))
 ((c_struct_def name: (_) @cursor) @endable @indent (#endwise! "end"))
 
-((annotation_def name: (_) @cursor) @endable @indent (#endwise! "end"))
+((method_def name: (_) @cursor params: (param_list)? @cursor) @endable @indent (#endwise! "end"))
+((macro_def name: (_) @cursor params: (param_list)? @cursor) @endable @indent (#endwise! "end"))
+((fun_def name: (_) @cursor params: (param_list)? @cursor) @endable @indent (#endwise! "end"))
 
-((fun_def name: (_) @cursor parameters: (_)? @cursor) @endable @indent (#endwise! "end"))
-
-((method_def name: (_) @cursor parameters: (_)? @cursor) @endable @indent (#endwise! "end"))
-
-((macro_def name: (_) @cursor) @endable @indent (#endwise! "end"))
-
-((while condition: (_) @cursor) @endable @indent (#endwise! "end"))
-
-((until condition: (_) @cursor) @endable @indent (#endwise! "end"))
-
-((for value: (_) @cursor) @endable @indent (#endwise! "end"))
-
-((block "do" @cursor parameters: (_)? @cursor) @endable @indent (#endwise! "end"))
-
-((if condition: (_) @cursor) @endable @indent (#endwise! "end"))
-
-((begin "begin" @cursor) @endable @indent (#endwise! "end"))
-
-((unless condition: (_) @cursor) @endable @indent (#endwise! "end"))
-
-((case value: (_) @cursor) @endable @indent (#endwise! "end"))
-
+((while cond: (_) @cursor) @endable @indent (#endwise! "end"))
+((until cond: (_) @cursor) @endable @indent (#endwise! "end"))
+((if cond: (_) @cursor) @endable @indent (#endwise! "end"))
+((unless cond: (_) @cursor) @endable @indent (#endwise! "end"))
+((begin) @cursor @endable @indent (#endwise! "end"))
+((case) @cursor @endable @indent (#endwise! "end" nil "end" 0))
 ((select) @cursor @endable @indent (#endwise! "end"))
 
-((ERROR ("module" @indent . (_) @cursor)) (#endwise! "end"))
+((block "do" @cursor params: (param_list)? @cursor) @endable @indent (#endwise! "end"))
 
-((ERROR ("class" @indent . (_) @cursor . (_)? @cursor)) (#endwise! "end"))
+((heredoc_start) @cursor @endable @indent (#endwise! "" @cursor "heredoc_end" 0 "<<\\~\\?\\zs.*"))
 
-((ERROR ("struct" @indent . (_) @cursor)) (#endwise! "end"))
-
-((ERROR ("enum" @indent . (_) @cursor)) (#endwise! "end"))
-
-((ERROR ("lib" @indent . (_) @cursor)) (#endwise! "end"))
-
-((ERROR ("def" @indent . (_) @cursor . (_)? @cursor)) (#endwise! "end"))
-
-((ERROR ("fun" @indent . (_) @cursor . (_)? @cursor)) (#endwise! "end"))
-
-((ERROR ("macro" @indent . (_) @cursor)) (#endwise! "end"))
-
-((ERROR ("while" @indent . (_) @cursor . "do"? @cursor)) (#endwise! "end"))
-
-((ERROR ("until" @indent . (_) @cursor . "do"? @cursor)) (#endwise! "end"))
-
-((ERROR ("for" @indent . (_) . "in" . (_) @cursor . "do"? @cursor)) (#endwise! "end"))
-
-((ERROR ("do" @cursor @indent . (_)? @cursor)) (#endwise! "end"))
-
+((ERROR ("module" @indent . [(constant) (generic_type)] @cursor)) (#endwise! "end"))
+((ERROR ("class" @indent . [(constant) (generic_type)] @cursor . [(constant) (generic_instance_type)]? @cursor)) (#endwise! "end"))
+((ERROR ("struct" @indent . [(constant) (generic_type)] @cursor . [(constant) (generic_instance_type)]? @cursor)) (#endwise! "end"))
+((ERROR ("enum" @indent . (constant) @cursor)) (#endwise! "end"))
+((ERROR ("lib" @indent . [(constant) (generic_type)] @cursor)) (#endwise! "end"))
+((ERROR ("def" @indent . [(identifier) (operator)] @cursor . (param_list)? @cursor)) (#endwise! "end"))
+((ERROR ("macro" @indent . [(identifier) (operator)] @cursor . (param_list)? @cursor)) (#endwise! "end"))
+((ERROR ("fun" @indent . [(identifier) (constant)] @cursor . (param_list)? @cursor)) (#endwise! "end"))
+((ERROR ("while" @indent . (_) @cursor)) (#endwise! "end"))
+((ERROR ("until" @indent . (_) @cursor)) (#endwise! "end"))
+((ERROR ("if" @indent . (_) @cursor . (then)? @cursor)) (#endwise! "end"))
+((ERROR ("unless" @indent . (_) @cursor . (then)? @cursor)) (#endwise! "end"))
 ((ERROR ("begin" @cursor @indent)) (#endwise! "end"))
-
-((ERROR ("if" @indent . (_) @cursor . "then"? @cursor)) (#endwise! "end"))
-
-((ERROR ("unless" @indent . (_) @cursor . "then"? @cursor)) (#endwise! "end"))
-
-((ERROR ("case" @indent)) (#endwise! "end"))
+((ERROR ("do" @cursor @indent . (param_list)? @cursor)) (#endwise! "end"))
+((ERROR ("case" @cursor @indent)) (#endwise! "end"))
+((ERROR ("select" @cursor @indent)) (#endwise! "end"))
